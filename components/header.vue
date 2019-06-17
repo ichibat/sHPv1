b0e0f4e8d0799ff502173261806e0ed8024af940
tim ichiba <ichibat@gmail.com>
2019-06-18 07:25:07 +0900

    after a while

--- components/header.vue
+++ components/header.vue
@@ -1,48 +1,53 @@
 <template>
-  <nav class="navbar is-white">
-    <div class="container">
-      <div class="navbar-brand">
-        <nuxt-link to="/" class="navbar-item" >
-          <img src="~/assets/sumire_rogo.png" alt="すみれホームケアクリニックロゴ" width="28" height="28">
+  <div>
+    <h1 class="title">練馬およびその周辺地域の在宅医療 すみれホームケアクリニック－患者様の「おうちで過ごしたい」を医師が24時間サポートします－</h1>
+    <nav class="navbar is-white">
+      <div class="container">
+        <div class="navbar-brand">
+          <nuxt-link to="/" class="navbar-item" >
+            <img src="~/assets/sumire_rogo.png" alt="すみれホームケアクリニックロゴ" width="28" height="28">
+            <span>すみれホームケアクリニック</span>
+          </nuxt-link>
 
-          <span>すみれホームケアクリニック</span>
-        </nuxt-link>
-
-        <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-
-      <div id="navbarExampleTransparentExample" class="navbar-menu">
-        <div class="navbar-start">
-          <nuxt-link to="/" class="navbar-item">トップページ</nuxt-link>
-          <nuxt-link to="/about" class="navbar-item">クリニックのご紹介</nuxt-link>
-          <nuxt-link to="/contacts" class="navbar-item">お問い合わせ</nuxt-link>
-          <nuxt-link to="/blog" class="navbar-item">ブログ</nuxt-link>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link" href="/documentation/overview/start/">More</a>
-            <div class="navbar-dropdown is-boxed">
-              <a class="navbar-item" href="/documentation/overview/start/">Mechandise</a>
-              <a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">Extras</a>
-              <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">Media</a>
-            </div>
+          <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
         </div>
 
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="field">
-              <div class="control">
-                <input class="input" type="text" placeholder="search . . .">
+        <div id="navbarExampleTransparentExample" class="navbar-menu">
+          <div class="navbar-start">
+            <nuxt-link to="/" class="navbar-item">トップページ</nuxt-link>
+            <nuxt-link to="/about" class="navbar-item">クリニックのご紹介</nuxt-link>
+            <nuxt-link to="/contacts" class="navbar-item">お問い合わせ</nuxt-link>
+            <nuxt-link to="/blog" class="navbar-item">ブログ</nuxt-link>
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link" href="/documentation/overview/start/">More</a>
+              <div class="navbar-dropdown is-boxed">
+                <a class="navbar-item" href="/documentation/overview/start/">Mechandise</a>
+                <a
+                  class="navbar-item"
+                  href="https://bulma.io/documentation/modifiers/syntax/"
+                >Extras</a>
+                <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">Media</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="field">
+                <div class="control">
+                  <input class="input" type="text" placeholder="search . . .">
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <script>
