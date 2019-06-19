f0d3037c68b332697b062c42ffc88cde8c86f1b1
tim ichiba <ichibat@gmail.com>
2019-06-19 22:13:25 +0900

    with sass

--- components/header.vue
+++ components/header.vue
@@ -1,46 +1,41 @@
 <template>
   <div>
-    <h1 class="title">練馬およびその周辺地域の在宅医療 すみれホームケアクリニック－患者様の「おうちで過ごしたい」を医師が24時間サポートします－</h1>
-    <nav class="navbar is-white">
-      <div class="container">
-        <div class="navbar-brand">
-          <nuxt-link to="/" class="navbar-item" >
-            <img src="~/assets/sumire_rogo.png" alt="すみれホームケアクリニックロゴ" width="28" height="28">
-            <span>すみれホームケアクリニック</span>
-          </nuxt-link>
+    <!-- <h1 class="title">練馬およびその周辺地域の在宅医療 すみれホームケアクリニック－患者様の「おうちで過ごしたい」を医師が24時間サポートします－</h1> -->
+    <nav>
+      <div class="b2-1pad">
+        <nuxt-link to="/">
+          <img src="~/assets/sumire_rogo.png" alt="すみれホームケアクリニックロゴ" width="70" height="auto">
+          <span>すみれホームケアクリニック</span>
+        </nuxt-link>
 
-          <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
-            <span></span>
-            <span></span>
-            <span></span>
+        <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div id="navbarExampleTransparentExample" class="navbar-menu">
+        <div class="navbar-start">
+          <nuxt-link to="/" class="navbar-item">トップページ</nuxt-link>
+          <nuxt-link to="/about" class="navbar-item">クリニックのご紹介</nuxt-link>
+          <nuxt-link to="/contacts" class="navbar-item">お問い合わせ</nuxt-link>
+          <nuxt-link to="/blog" class="navbar-item">ブログ</nuxt-link>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link" href="/documentation/overview/start/">More</a>
+            <div class="navbar-dropdown is-boxed">
+              <a class="navbar-item" href="/documentation/overview/start/">Mechandise</a>
+              <a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">Extras</a>
+              <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">Media</a>
+            </div>
           </div>
         </div>
 
-        <div id="navbarExampleTransparentExample" class="navbar-menu">
-          <div class="navbar-start">
-            <nuxt-link to="/" class="navbar-item">トップページ</nuxt-link>
-            <nuxt-link to="/about" class="navbar-item">クリニックのご紹介</nuxt-link>
-            <nuxt-link to="/contacts" class="navbar-item">お問い合わせ</nuxt-link>
-            <nuxt-link to="/blog" class="navbar-item">ブログ</nuxt-link>
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link" href="/documentation/overview/start/">More</a>
-              <div class="navbar-dropdown is-boxed">
-                <a class="navbar-item" href="/documentation/overview/start/">Mechandise</a>
-                <a
-                  class="navbar-item"
-                  href="https://bulma.io/documentation/modifiers/syntax/"
-                >Extras</a>
-                <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">Media</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="field">
-                <div class="control">
-                  <input class="input" type="text" placeholder="search . . .">
-                </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="field">
+              <div class="control">
+                <input class="input" type="text" placeholder="search . . .">
               </div>
             </div>
           </div>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,66 @@
-import pkg from './package'
+import pkg from "./package";
 
 export default {
-  mode: 'universal',
+  mode: "universal",
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'すみれホームケアクリニック',
+    title: "すみれホームケアクリニック",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
     ],
     link: [
       // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css' },
-      { rel: "stylesheet", href: "https://use.fontawesome.com/releases/v5.8.1/css/all.css" },
-      { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.2.1/font-awesome-animation.css" }
+      {
+        rel: "stylesheet",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css"
+      },
+      {
+        rel: "stylesheet",
+        href: "https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.2.1/font-awesome-animation.css"
+      }
     ]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
 
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: ["@/assets/css/main.scss"],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt',
+    "bootstrap-vue/nuxt"
   ],
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
