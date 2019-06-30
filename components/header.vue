73e58297b5b41604e356e103c11461fb7cb29981
tim ichiba <ichibat@gmail.com>
2019-06-30 18:02:14 +0900

    small modificatiojn

--- components/header.vue
+++ components/header.vue
@@ -1,24 +1,23 @@
 <template>
-  <div class="l-container">
+  <div>
     <header class="l-header">
-      <span class="c-header__leftList">
-        <nuxt-link to="/">
-          <img src="~/assets/sumire_rogo.png" alt="すみれホームケアクリニックロゴ" class="c-header__logo">
-          <span>すみれホームケアクリニック</span>
-        </nuxt-link>
-      </span>
-
-      <ul class="c-header__rightList">
-        <li class="c-header__item">
+      <ul class="c-header">
+        <li class="c-header__item--left">
+          <nuxt-link to="/">
+            <img src="~/assets/sumire_rogo.png" alt="すみれホームケアクリニックロゴ" class="c-header__logo">
+            <span class="c-header__name">すみれホームケアクリニック</span>
+          </nuxt-link>
+        </li>
+        <li class="c-header__item--right">
           <nuxt-link to="/">トップページ</nuxt-link>
         </li>
-        <li class="c-header__item">
+        <li class="c-header__item--right">
           <nuxt-link to="/about">クリニックのご紹介</nuxt-link>
         </li>
-        <li class="c-header__item">
+        <li class="c-header__item--right">
           <nuxt-link to="/contacts">お問い合わせ</nuxt-link>
         </li>
-        <li class="c-header__item">
+        <li class="c-header__item--right">
           <nuxt-link to="/blog">ブログ</nuxt-link>
         </li>
       </ul>
